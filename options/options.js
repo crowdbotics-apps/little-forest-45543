0b5a32df7ba983abe736f89dcf82922aa6d0c7b0
Crowdbotics <team@crowdbotics.com>
2024-01-25 15:26:57 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "little-forest-45543",
+  url: "https://little-forest-45543.botics.co",
+  api: "https://little-forest-45543.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
